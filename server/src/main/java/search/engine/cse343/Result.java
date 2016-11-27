262e89639ec81e39c1a7c3876965f354bbd5ae19
bekircanagaoglu <bagaoglu@gtu.edu.tr>
2016-11-27 23:38:27 +0300

    crawler

---
+++ server/src/main/java/search/engine/cse343/Result.java
@@ -0,0 +1,39 @@
+package search.engine.cse343;
+
+public class Result {
+
+    private String url;
+    private String title;
+
+    public Result(String title, String url){
+
+        this.url = url;
+        this.title = title;
+    }
+
+    public String getUrl(){
+
+        return url;
+    }
+
+    public String getTitle(){
+
+        return title;
+    }
+
+    @Override
+    public String toString(){
+
+        return url;
+    }
+
+    @Override
+    public boolean equals(Object o){
+
+        if(o instanceof Result == false)
+
+            return false;
+
+        return getUrl().equals(((Result)o).getUrl());
+    }
+}
