025a48315bb0c5d0bdd0a21858d50cfe0fbd5f4a
Caner Bakar <feanor924@gmail.com>
2016-12-06 17:22:55 +0200

    Update ScreenshotTaker.java

--- server/src/main/java/search/engine/cse343/ScreenshotTaker.java
+++ server/src/main/java/search/engine/cse343/ScreenshotTaker.java
@@ -4,21 +4,21 @@ public class ScreenshotTaker{
 	public static void takeScreenshot(String url, String path){
 		
 		String[] c = new String[] {"/bin/bash", "-c", "phantomjs /home/canerbakar/Desktop/github.js " 
-        + url + path};
+        	+ url + path};
 		
 		Process proc = null;
 
 		try {
-            proc = new ProcessBuilder(c).start();
-        } catch (IOException ex) {
-            System.out.printf("Problem with Process");
-        }
+            		proc = new ProcessBuilder(c).start();
+        	} catch (IOException ex) {
+            	System.out.printf("Problem with Process");
+        	}
 
-        try{
-            proc.waitFor();
-        }catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+		try{
+		    proc.waitFor();
+		}catch (InterruptedException e) {
+		    e.printStackTrace();
+		}
 	
 	
 	}
